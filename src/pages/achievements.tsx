7f92406d0eb2c4b675e25b54cf675c7e41d97ee1
agarogo <87768035+agarogo@users.noreply.github.com>
2025-05-28 22:47:23 +0900

    Updat2

--- src/pages/achievements.tsx
+++ src/pages/achievements.tsx
@@ -4,13 +4,13 @@ export default function Achievements() {
       <h2 className="text-4xl text-center mb-6 mon">Достижения</h2>
       <div className="space-y-4 max-w-4xl mx-auto">
         <div className="bg-[#86F37D] text-[#0A1044] p-4 rounded-[20px] mt-10">
-          Сертификат за прохождение каникулярного проекта ПИОС СВЯЗЬ и ЯРОО ВОО "МИР" у прохождение каникулярного мероприятия "ОФИС"
+          Сертификат за прохождение каникулярного проекта ПИОС СВЯЗЬ и ЯРОО ВОО &quot;МИР&quot; и прохождение каникулярного мероприятия &quot;ОФИС&quot;
         </div>
         <div className="bg-[#F9E35D] text-[#0A1044] p-4 rounded-[20px]">
-          Сертификат за участие в рейтинговом смотре "АРТКУ"
+          Сертификат за участие в рейтинговом смотре &quot;АРТКУ&quot;
         </div>
         <div className="bg-[#5C47FD] text-white p-4 rounded-[20px]">
-          Диплом 1 степени НИК ЧАР в будущем Word Skills Junior по направлению - Флористика
+          Диплом 1 степени НИК ЧАР в будущем World Skills Junior по направлению — Флористика
         </div>
       </div>
     </section>
